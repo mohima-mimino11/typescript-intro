1f7e717b15522495a5c66e5d91bb08c723742903
mohima-mimino11 <mohimahoque22@gmail.com>
2025-04-28 22:40:44 +0600

    data-types started

---
+++ data-types.ts
@@ -0,0 +1,9 @@
+// Integer number
+let age: number = 30;
+// Float number
+let marks: number = 30.5;
+console.log(age, marks);
+let firstName : string = "Taylor";
+let lastName : string = "Swift";
+let fullName : string = `${firstName}  ${lastName}`;
+console.log(fullName);
